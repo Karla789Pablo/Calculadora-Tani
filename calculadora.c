81fe19317fff167c45338dc80d783a74c0a3d8dc
Karla789Pablo <137184730+Karla789Pablo@users.noreply.github.com>
2024-09-03 12:59:24 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -3,7 +3,7 @@ void titulo();
 void menu(int *);
 void opciones(int);
 
-VOID suma();
+void suma();
 
 void fin();
 
@@ -21,5 +21,44 @@ void titulo(){
 }
 
 void menu(int * opc){
-  printf("\n\n Selecciona que hacer");
+  printf("\n\n Selecciona que hacer:\n");
+  printf("  1. Sumar\n");
+  printf("  2. Restar\n");
+  printf("  3. Multiplicar\n");
+  printf("  4. Dividir\n");
+  printf("  5. Modulo\n");
+  printf("  6. Salir del programa\n  ");
+  scanf("%d", opc); 
+}
+
+void opciones(int opc){
+  switch(opc){
+    case 1:
+      suma();
+      break;
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      fin();
+      break;
+    default:
+      printf("Opcion no valida\n);
+  }
+}
+
+void suma(){
+  printf("\t SUMA");
+  int s1, s2, t;
+  printf("\n Ingresa el primer valor:");
+  scanf("%d", &s1);
+  printf("\n Ingresa el segundo valor:");
+  scanf("%d", &s2);
+  t = s1 + s2;
+  printf("\n El resultado es %d", t);
+}
+
+void fin(){
+  printf("\n  Fin del programa.");
 }
