3dd4ccbdb9bf4b545bc4854a39d1bfbcc875ed0c
Mels05 <108698482+Mels05@users.noreply.github.com>
2024-09-03 12:46:27 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -24,4 +24,13 @@ void menu(int * opc){
   printf("\n\n Selecciona que hacer");
 }
 
-void resta
+void resta() {
+  printf("RESTA");
+  int n1, n2,resultado;
+printf("Ingresa un valor");
+ scanf("%d", &n1);
+printf("Ingresa otro valor");
+ scanf("%d", &n2);
+resultado=n1-n2;
+printf("Resultado: %d", resultado);
+}
