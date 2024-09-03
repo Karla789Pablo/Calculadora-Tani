36afe4adece1740f623b4e8e30589e4decedb3ea
MrRaindrp <134899310+MrRaindrp@users.noreply.github.com>
2024-09-03 12:38:22 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -7,9 +7,19 @@ void fin();
 
 int main(){
   int opc;
+  printf("Menú:\n");
+  printf("1. Suma\n");
+  printf("2. Resta\n");
+  printf("3. Multiplicación\n");
+  printf("4. División\n");
+  printf("5. Módulo\n");
+  printf("6. Salir\n");
+  printf("Ingrese una opción (1-6): ");
+  scanf("%d", &opcion);
   titulo();
   while(opc!=5){
     menu(&opc);
     opciones(opc);
+    if(opc = 5
   }
 }
