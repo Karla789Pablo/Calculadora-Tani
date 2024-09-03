df2831181cc1a01256a70f45c8a067cba0d5b829
MrRaindrp <134899310+MrRaindrp@users.noreply.github.com>
2024-09-03 12:42:12 -0600

    Update calculadora.c
    Cambio menor de tabulación

--- calculadora.c
+++ calculadora.c
@@ -43,14 +43,14 @@ int main(){
     	
   }
     
-  printf("Menú:\n");
-  printf("1. Suma\n");
-  printf("2. Resta\n");
-  printf("3. Multiplicación\n");
-  printf("4. División\n");
-  printf("5. Módulo\n");
-  printf("6. Salir\n");
-  printf("Ingrese una opción (1-6): ");
-  scanf("%d", &opc);
-    
+    printf("Menú:\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicación\n");
+    printf("4. División\n");
+    printf("5. Módulo\n");
+    printf("6. Salir\n");
+    printf("Ingrese una opción (1-6): ");
+    scanf("%d", &opc);
+      
 }
