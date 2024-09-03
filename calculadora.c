2679cc738123d2eeebac99e22f286da653e0195a
Karla789Pablo <137184730+Karla789Pablo@users.noreply.github.com>
2024-09-03 12:46:19 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+
 void titulo();
 void menu(int *);
 void opciones(int);
 
-VOID suma();
+void suma();
 
 void fin();
 
@@ -21,5 +22,22 @@ void titulo(){
 }
 
 void menu(int * opc){
-  printf("\n\n Selecciona que hacer");
+  printf("\n\n Selecciona que hacer:\n");
+  printf("1. Sumar\n");
+  printf("2. Restar\n");
+  printf("3. Multiplicar\n");
+  printf("4. Dividir\n");
+  printf("5. Modulo\n");
+  printf("\n");
+}
+
+void suma(){
+  printf("\t SUMA");
+  int s1, s2, t;
+  printf("\n Ingresa el primer valor:");
+  scanf("%d", &s1);
+  printf("\n Ingresa el segundo valor:");
+  scanf("%d", &s2);
+  t = s1 + s2;
+  printf("\n El resultado es %d", t);
 }
