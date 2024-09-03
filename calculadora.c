a7b30c5fdf434ec6d7922c407f9396fcdea73ab7
Mels05 <108698482+Mels05@users.noreply.github.com>
2024-09-03 12:59:57 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -3,27 +3,11 @@ void titulo();
 void menu(int *);
 void opciones(int);
 
-VOID suma();
 void resta();
 
-void fin();
-
-int main(){
-  int opc;
-  titulo();
-  while(opc!=5){
-    menu(&opc);
-    opciones(opc);
-  }
-}
-
-void titulo(){
-  printf("\t &&& CALCULADORA &&&");
-}
-
-void menu(int * opc){
-  printf("\n\n Selecciona que hacer");
-}
+case 2:
+  resta():
+    break;
 
 void resta() {
   printf("RESTA");
