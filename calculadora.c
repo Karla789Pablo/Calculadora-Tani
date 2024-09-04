c08fc4096cd242089cbd6c8b649418f322477e17
MrRaindrp <134899310+MrRaindrp@users.noreply.github.com>
2024-09-03 18:22:02 -0600

    Update calculadora.c
    Usando el main cambiando

--- calculadora.c
+++ calculadora.c
@@ -1,6 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
 
+void titulo();
+void menu(int *);
+void opciones(int);
 void multiplicacion();
 void modulo();
+
+void suma();
+
+void fin();
+
+int main(){
+  int opc;
+  titulo();
+  while(opc!=6){
+    menu(&opc);
+    opciones(opc);    
+    Sleep(5000);
+    system("cls");
+
+  }
+}
+
+void titulo(){
+  printf("\t &&& CALCULADORA &&&");
+}
+
+void menu(int * opc){
+  printf("\n\n Selecciona que hacer:\n");
+  printf("  1. Sumar\n");
+  printf("  2. Restar\n");
+  printf("  3. Multiplicar\n");
+  printf("  4. Dividir\n");
+  printf("  5. Modulo\n");
+  printf("  6. Salir del programa\n  ");
+  scanf("%d", opc); 
+}
+
+void opciones(int opc){
+  switch(opc){
+    case 1:
+      suma();
+      break;
+    case 2:
+    case 3:
+        multiplicar();
+    case 4:
+    case 5:
+        modulo();
+    case 6:
+      fin();
+      break;
+    default:
+      printf("Opcion no valida\n);
+  }
+}
+
+void suma(){
+  printf("\t SUMA");
+  int s1, s2, t;
+  printf("\n Ingresa el primer valor:");
+  scanf("%d", &s1);
+  printf("\n Ingresa el segundo valor:");
+  scanf("%d", &s2);
+  t = s1 + s2;
+  printf("\n El resultado es %d", t);
+}
+
+void fin(){
+  printf("\n  Fin del programa.");
+}
 
 void modulo(){
       int first, second;
